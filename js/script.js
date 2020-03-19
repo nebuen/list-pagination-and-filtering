893445ceb3338c551029d8179fc11918e5463a7b
Patrick Ebuen <norman.ebuen@gmail.com>
2020-03-19 03:10:50 -0400

    update functions
    fix adding class name , removing and adding 'active' class name to the anchor selected

--- js/script.js
+++ js/script.js
@@ -39,9 +39,7 @@ const showPage = (list, page) => {
    }
    return listOfStudents;
 }
-// console.log(showPage(studentList, 6));
-// console.log(studentList.length);
-
+console.log(showPage(studentList, 1));
 /*** 
    Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
@@ -51,31 +49,48 @@ const appendPageLinks = (list) => {
    for(let i = 0; i < list.length; i += 9){
       pages += 1;
    }
+   // create div with classname 'pagination'
    let div = document.createElement('DIV');
    div.className = "pagination";
+   // get the div with classname 'page' and append the div.pagination we created
    let div_page = document.querySelector('.page');
    div_page.appendChild(div);
+   //create ul element and append to newly created div.pagination
    let ul = document.createElement('UL');
    div.appendChild(ul);
-   let a = document.getElementsByTagName('a');
-   
+   //loop through pages to know the number of 'li' will be added to 'ul'
    for(let i = 0; i < pages; i += 1){
+      //create LI with a=href'#'
       let li = document.createElement('LI');
-    
       let a = document.createElement('A');
       a.href = '#';
-      console.log(pages[0]);
       a.innerHTML = i + 1;
       li.appendChild(a);
-      a.addEventListener('click', showPage(list, pages));
       ul.appendChild(li);
-      for(let i = 0; i < div.length; i += 1){
-         a[i].classList.remove("active");
-      }
-      console.log(li.childNodes);
-      a.event.target.className = "active";
+      // a.addEventListener('click', function(e){
+      //    showPage(list, pages);
+      //    e.target.className ='active';
+         
+      // });
+      // console.log(li.childNodes[0]);
+      // const event = a.target;
+      // event.classList.add('active');
       // li.event.target = li.classList.add('active');
    }
+   //get all anchor tag then add class 'active' on the first anchor tag only
+   a = document.querySelectorAll('div.pagination ul li a');
+   a[0].className = 'active';
+   // loop through anchor tag and add all 'a' addEventListener and another loop to remove class 'active' to all anchor tag
+   for(let i = 0; i < a.length; i += 1){
+      a[i].addEventListener('click', function(e){
+         //removing 'active' class to all 'a'
+         for(let j = 0; j < a.length; j += 1){
+            a[j].classList.remove('active');
+         }
+         e.target.className = 'active';
+      });
+   }
+   
    
    
 }
